4f164c7780f7cdbbcd6ca988cc5ba47e4377a362
Ubuntu <ubuntu@ip-172-31-47-122.us-east-2.compute.internal>
2019-04-22 00:42:58 +0000

    Optimize insertion

--- generateData.js
+++ generateData.js
@@ -1,43 +1,50 @@
 const fastcsv = require('fast-csv');  
 const fs = require('fs');  
-const faker = require('faker')
+// const faker = require('faker')
 
-const randomInt = (min,max) => {
-  return Math.floor(Math.random()*(max-min+1)+min);
-}
+// const randomInt = (min,max) => {
+//   return Math.floor(Math.random()*(max-min+1)+min);
+// }
 
-let array = []
+// let array = ['poiuytrewq', 'qwertyuio', '123456789', ]
 
-function createArray() {
-	for (let i = 0; i < 100; i++) {
-		array.push(faker.lorem.sentence())
-	}
-	return array
-}
+var start = new Date()
 
-createArray()
+// function createArray() {
+// 	for (let i = 0; i < 100; i++) {
+// 		array.push(faker.lorem.sentence())
+// 	}
+// 	return array
+// }
+
+// createArray()
 
 const generateData = () => {
+
   const arr = []
-  for (let i = 0; i < 10000000; i++) {
+  for (let i = 0; i < 1000000; i++) {
     let data = {
     id: i,
 	  recImg:  'https://loremflickr.com/320/240',
-	  recDetails: array[randomInt(0, 19)], 
-	  recTitle:  array[randomInt(0, 19)],
-	  recCost:  randomInt(0, 100), 
-	  recRating: randomInt(0, 100),
-	  recratingCount: randomInt(0, 100), 
-	  roomId: randomInt(0, 100),
+	  recDetails:'poiuytrewq', 
+	  recTitle:  'qwertyuio',
+	  recCost:  12234,  
+	  recRating: 42323432,
+	  recratingCount: 32532, 
+	  roomId: 32423,
     }
     arr.push(data)
-  }
+	}
   return arr
-
 }
 
+
 let data = generateData()
-const ws = fs.createWriteStream("data10m.csv");  
+const ws = fs.createWriteStream(`data4.csv`);  
 fastcsv  
   .write(data, { headers: true })
-  .pipe(ws);
+	.pipe(ws);
+
+	var end = new Date() - start
+	console.log('took:  %dms', end)
+	console.log(process.memoryUsage());
--- mysql.js
+++ mysql.js
@@ -4,19 +4,20 @@ let MysqlPoolBooster = require('mysql-pool-booster');
 mysql = MysqlPoolBooster(mysql);
 
 let pool = mysql.createPool({
- host: 'localhost',
- user: 'root',
- password: '',
- database: 'recs'
+    host: 'localhost',
+    user: 'root',
+password: 'root',
+    database: 'recs'
 });
 
-pool.getConnection(function (err) {
- if (err) {
-  return console.error('error: ' + err.message);
- }
- console.log('Connected to the MySQL server.');
 
- let createRecs = `CREATE TABLE IF NOT EXISTS recommendations (
+pool.getConnection(function (err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+    console.log('Connected to the MySQL server.');
+
+    let createRecs = `CREATE TABLE IF NOT EXISTS recommendations (
 		id INT,
 		recImg VARCHAR (255),
 		recDetails VARCHAR (255),
@@ -27,40 +28,67 @@ pool.getConnection(function (err) {
 		roomId INT,
 		INDEX (id)
   ) 
-  ENGINE=InnoDB`
+  ENGINE= MyISAM;`
 
- pool.query(`USE recs;`)
+    pool.query(`USE recs;`)
 
- pool.query(createRecs, function (err, results, fields) {
-  if (err) {
-   console.log(err.message);
-  }
- })
+    pool.query(createRecs, function (err, results, fields) {
+        if (err) {
+            console.log(err.message);
+        }
+    })
 })
 
-function save() {
- var start = new Date()
+async function save() {
+    var start = new Date()
+    pool.query(`SET AUTOCOMMIT = 0;`)
+    pool.query(`SET foreign_key_checks=0`)
+    pool.query(`SET unique_checks=0`)
+    pool.query(`SET sql_log_bin = 0`)
+    // pool.query(`SET SESSION tx_isolation='READ-UNCOMMITTED'`)
 
- pool.query(`LOAD DATA LOCAL INFILE 'data1m.csv' 
+    pool.query(`LOAD DATA LOCAL INFILE 'data4.csv' 
 				INTO TABLE recommendations 
 				FIELDS TERMINATED BY ',' 
-				LINES TERMINATED BY '\n';`, (err, result, fields) => {
+                LINES TERMINATED BY '\n'
+                ;`, (err, result, fields) => {
 
-  if (err) {
-   console.error('err on insert', err)
-  }
-  var end = new Date() - start
-  console.log('took:  %dms', end)
- })
+        if (err) {
+            console.error('err on insert', err)
+        } 
+    var end = new Date() - start
+        console.log('took:  %dms', end)
+    })
+    pool.query(`SET foreign_key_checks=0`)
+    pool.query(`SET unique_checks=0`)
+
 }
+
 
 let find = (id) => {
- var start = new Date()
- pool.query(`select * from recommendations where id ='${id}'`, (err, res) => {
-  var end = new Date() - start
-  console.log(res)
-  console.log('took:  %dms', end)
- })
+
+    pool.query(`select * from recommendations where id ='${id}'`, (err, res) => {
+        console.log(res)
+
+    })
 }
 
+// find(9999999)
+ for (let i = 0; i < 9; i++) {
+// // {
+// var start = new Date()
+ save()
+//.then(()=> {
+//     var end = new Date() - start
+//     console.log('took:  %dms', end)
+ }
+// console.log(process.memoryUsage());
+// var end = new Date() - start
+// console.log('took:  %dms', end)
 
+
+//  }
+// pool.query(`drop table recommendations`)
+// pool.query(`select * from recommendations where id = 99999`, (err, res)=>{
+//     console.log(res)
+// }) 
